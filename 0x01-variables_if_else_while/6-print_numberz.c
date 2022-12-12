704ded85bf7be28974841e0b913d1e6895a4e521
Adebambo123 <adebambosimisoluwa1@gmail.com>
2022-12-12 07:00:31 -0800

    c-lang

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ * Return: (0)
+*/
+int main(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+		putchar((num % 10) + '0');
+
+	putchar('\n');
+	return (0);
+}
